11df2f82e31d06372379307dd459de36277f4733
wlsdud2194 <wlsdud2194@naver.com>
2020-01-03 15:14:04 +0900

    Create Modal.stories.tsx

---
+++ src/components/Modal/Modal.stories.tsx
@@ -0,0 +1,12 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+import Modal from './Modal';
+
+export default {
+  title: 'components|Button/Button',
+  component: Modal,
+  parameters: {
+    componentSubtitle: 'modal',
+  },
+};
+
